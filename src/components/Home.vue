228aae517dbe1f2ad4e36425a29e7c8233587bf4
netrc <ric@netrc.com>
2021-12-31 18:37:20 +0000

    first home carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,29 @@
 <template>
   <v-container class='ctest'>
-  <v-row>
-        <h1> HOME </h1>
-  </v-row>
-  <v-row>
+
+    <v-carousel height="600">
+
+      <v-carousel-item>
+        <v-row class="fill-height" align="center" justify="center">
         <v-img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Wootton_Warwen_Church-1.jpg"></v-img>
-  </v-row>
- </v-container>
+        </v-row>
+      </v-carousel-item>
+
+      <v-carousel-item>
+        <v-row class="fill-height" align="center" justify="center">
+        <v-img src="https://www.britainexpress.com/images/attractions/editor/Wootton-Wawen-5973.jpg"></v-img>
+        </v-row>
+      </v-carousel-item>
+
+      <v-carousel-item>
+        <v-row class="fill-height" align="center" justify="center">
+        <v-img src="https://lh4.googleusercontent.com/-mpUF1M2JjZo/UChFVkeBWaI/AAAAAAAAMT4/o9H9dp9CSLA/s800/VLC-7-a.jpg"></v-img>
+        </v-row>
+      </v-carousel-item>
+    
+    </v-carousel>
+
+  </v-container>
 </template>
 
 <script>
